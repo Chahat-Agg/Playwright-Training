be537fc1f38acde562d855284eca8e2c615d9f31
Chahat <chahat922001@gmail.com>
2026-01-13 13:25:21 +0530

    commiting auto complete file

--- autoComplete.spec.js
+++ autoComplete.spec.js
@@ -1,6 +1,20 @@
 import {test, expect} from '@playwright/test'
 
 test("auto complete value in dropdown", async ({page})=>{
-    await page.goto("https://demoapps.qspiders.com/ui/dropdown/multiSelect?sublist=1");
+
+    const searchValue= "in"
+    const selectValue= "China"
+
+
+    await page.goto("https://demoapps.qspiders.com/ui/dropdown/search?sublist=2")
     
+    await page.locator("(//div[@class=' css-1xc3v61-indicatorContainer'])[3]").click()
+    await page.keyboard.type('india')
+
+    await page.keyboard.press('Enter')
+    /*await page.keyboard.press('ArrowDown')
+    await page.keyboard.press('ArrowDown')
+    await page.keyboard.press('Enter')*/
+    await page.waitForTimeout(10000);
+
 })
