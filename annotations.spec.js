df3b568fea096d2366657bc813ba0ab48ec8229f
Chahat <chahat922001@gmail.com>
2026-01-15 18:16:07 +0530

    annotations

--- annotations.spec.js
+++ annotations.spec.js
@@ -1,21 +1,26 @@
 import {test, expect} from '@playwright/test'
 
 test("verify title", async ({page})=>{
-    test.fail();
+    test.slow();
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     const title=await page.title();
     expect(title).toBe('OrangeHRM')
 })
 
-test("verify URL", async ({page})=>{
+test.only("verify URL", async ({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     const current_URL=await page.url();
     expect(current_URL).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 })
 
-test("verify login button", async ({page})=>{
+test.only("verify login button", async ({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     const button=await page.locator("//button[@type='submit']")
     const button_text=await button.textContent()
     expect(button_text).toBe(' Login ')
 })
+
+//test.fail()
+//text.fixMe()
+//test.only()
+//test.slow()
